f872bf972d891d28dce1f5fc4e4dccbdd7def534
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-08-25 12:02:16 +0530

    Add files via upload

---
+++ mavenexamplenew/src/main/java/scs/BrowserStackHomePage.java
@@ -0,0 +1,22 @@
+package scs;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserStackHomePage {
+    WebDriver driver;
+	By Header=By.xpath("//h1");
+	By getStarted=By.xpath("//*[@id='signupModalButton']");
+	public BrowserStackHomePage(WebDriver driver) {
+		this.driver=driver;
+	}
+	public void veryHeader() {
+		String getheadertext=driver.findElement(Header).getText();
+		assertEquals("App & Browser Testing Made Easy", getheadertext);
+		}
+	public void clickOnGetStarted() {
+		driver.findElement(getStarted).click();
+		}
+}
---
+++ mavenexamplenew/src/test/java/scs/ManageOperation.java
@@ -0,0 +1,37 @@
+package scs;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ManageOperation {
+	WebDriver driver;
+	BrowserStackHomePage objBrowserStackHomePage;
+	BrowserStackSignUpPage objBrowserStackSignUpPage;
+	@BeforeTest
+	public void setup() {
+	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.get("https://www.browserstack.com/");
+	}
+
+	@Test(priority = 1)
+	public void navigate_to_homepage_click_on_getstarted() {
+	objBrowserStackHomePage = new BrowserStackHomePage(driver);
+	objBrowserStackHomePage.veryHeader();
+	objBrowserStackHomePage.clickOnGetStarted();
+	}
+
+	@Test(priority = 2)
+	public void enter_userDetails() {
+	objBrowserStackSignUpPage = new BrowserStackSignUpPage(driver);
+	objBrowserStackSignUpPage.veryHeader();
+	objBrowserStackSignUpPage.enterFullName("TestUser");
+	objBrowserStackSignUpPage.enterBusinessEmail("TestUser@gmail.com");
+	objBrowserStackSignUpPage.enterPasswrod("TestUserPassword");
+	}
+}
